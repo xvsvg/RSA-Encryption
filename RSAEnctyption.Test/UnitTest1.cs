6152b024c7cb155cb573e6fc3a080d1e5adfdc7e
xvsvg <pobedotriples@gmail.com>
2022-10-23 22:07:48 +0300

    feat: add UI, prime number generator and tests

---
+++ RSAEnctyption.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using RSAEnctyption;
+
+namespace RSAEnctyption.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            RSA rSA = new RSA();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
